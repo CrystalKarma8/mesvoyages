8285fa0998be1590bde1912746a66b8e25112456
Perrine Meyrignac <1234perrine@gmail.com>
2025-10-07 17:37:39 +0200

    Phase 2 : Premier test :  Hello World

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Description of AccueilController
+ *
+ * @author user
+ */
+class AccueilController {
+    //put your code here
+    #[Route('/', name: 'accueil')]
+    public function index(): Response{
+        return new Response('Hello World!');
+    }
+}
