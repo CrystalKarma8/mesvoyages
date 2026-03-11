fdf3a82a0e0f709eeaf624e0d9fde34c5fa3d681
Perrine Meyrignac <1234perrine@gmail.com>
2026-03-11 23:23:48 +0100

    Phase 20 : sécurité

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -48,11 +48,14 @@ public function __construct(VisiteRepository $repository){
     
     #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
     public function findAllEqual($champ, Request $request): Response{
-        $valeur = $request->get("recherche");
-        $visites = $this->repository->findByEqualValue($champ, $valeur);
-        return $this->render("pages/voyages.html.twig",[
-            'visites' => $visites
-        ]);
+        if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('token'))){
+             $valeur = $request->get("recherche");
+            $visites = $this->repository->findByEqualValue($champ, $valeur);
+            return $this->render("pages/voyages.html.twig",[
+                'visites' => $visites
+            ]);
+        }
+        return $this->redirectToRoute("voyages");
         
     }
     
