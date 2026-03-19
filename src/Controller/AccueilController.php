cfb442ff7e63106612834b86bb6317d522263907
Perrine Meyrignac <1234perrine@gmail.com>
2026-03-19 17:38:43 +0100

    Phase 21 : page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController {
     //put your code here
     #[Route('/', name: 'accueil')]
-    public function index(): Response{
-        return $this->render("pages/accueil.html.twig");
+    public function index(VisiteRepository $visiteRepository): Response {
+        
+        // On récupère les 2 derniers voyages triés par date décroissante
+        $derniersVoyages = $visiteRepository->findBy([], ['datecreation' => 'DESC'], 2);
+
+        return $this->render("pages/accueil.html.twig", [
+            'voyages' => $derniersVoyages
+        ]);
     }
 }
